a3d980d6c073c5a08858bd06f0d0c509189d93d8
Mike Goll <mike_g97@hotmail.com>
2020-01-14 16:23:25 -0800

    Added multiple monitor support for Polybar, added Flameshot to the Print Screen keybind, and reduced window gaps.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,5 +5,11 @@ killall -q polybar
 #Wait until the processes have been shut down
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-polybar -r example
+if type "xrandr"; then
+    for m in $(xrandr --query | grep "connected" | cut -d" " -f1); do
+	MONITOR=$m polybar --reload example &
+    done
+else
+    polybar -r example
+fi
 echo "Bars launched..."
